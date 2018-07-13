d8cf09ec5b217e9bffeee38954b740ddbdae5b7c
Grant Kazan <grantkazan@gmail.com>
2018-07-13 10:00:51 -0500

    implemented RESTful actions

--- app/controllers/singers_controller.rb
+++ app/controllers/singers_controller.rb
@@ -1,2 +1,38 @@
 class SingersController < ApplicationController
+  def index
+    @singers = Singer.all
+    render "index.json.jbuilder"
+  end
+
+  def show
+    @singer = Singer.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
+
+  def create
+    @singer = Singer.new(
+      name: params[:name],
+      genre: params[:genre],
+      born: params[:born],
+      )
+    @singer.save
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @singer = Singer.find_by(id: params[:id])
+    @singer.name = params[:name] || @singer.name
+    @singer.genre = params[:genre] || @singer.genre
+    @singer.born = params[:born] || @singer.born
+    @singer.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @singer = Singer.find_by(id: params[:id])
+    @singer.destroy
+    render json:{message: "Singer removed"}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/singers" => "singers#index"
+  get "/singers/:id" => "singers#show"
+  post "/singers" => "singers#create"
+  patch "/singers/:id" => "singers#update"
+  delete "/singers/:id" => "singers#destroy"
 end
